b29a7c205c2e8cc9b8ac9beeb822f77e088e4a5c
me.maani <chaudharysuraksha01@gmail.com>
2025-01-31 12:35:46 +0530

    Update Header.js
    Fixed the mess i made up while fixing the image issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Navbar from "./Navbar"; // Import Navbar for placing beside the logo
 import "./Header.css";
 
 const Header = () => {
     return (
-        <header className="header">
-            <div className="logo">
-                {/* Update the logo src path */}
-                <img
+        <header className="header-container">
+            <img
                     src={`${process.env.PUBLIC_URL}/logo.png`}  // Use process.env.PUBLIC_URL
                     alt="DIETY Logo"
                     className="logo-image"
                 />
-            </div>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/aboutus">About Us</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
-                </ul>
-            </nav>
+            <Navbar /> {/* Place the Navbar beside the logo */}
         </header>
     );
 };
