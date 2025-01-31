0ab183e1f7cc13c4e5e708bb3b296d00add0b26b
me.maani <chaudharysuraksha01@gmail.com>
2025-01-31 12:26:28 +0530

    Update App.js
    Made Home page visible by default

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
             <div className="App">
                 {/* Header */}
                 <Header />
-
+                <Home />
                 {/* Navbar */}
                 {/* <Navbar /> */}
 
